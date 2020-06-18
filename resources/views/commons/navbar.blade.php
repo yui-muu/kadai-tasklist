22a606f77de3d07b143bf390bb9b192e415c7cef
yui.murayama <ravu34zu@yahoo.co.jp>
2020-06-18 07:01:49 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,6 +12,10 @@
             <ul class="navbar-nav">
                 {{-- タスク作成ページへのリンク --}}
                 <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ログインページへのリンク --}}
+                <li><a href="#">Login</a></li>
             </ul>
         </div>
     </nav>
